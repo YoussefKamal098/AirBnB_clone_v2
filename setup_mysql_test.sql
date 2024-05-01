0a696f2daf3c8e3dba54b168167ea4619bfa1d41
YoussefKamal098 <kmalywsf098@gmail.com>
2024-05-01 13:05:57 +0300

    setup_mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- this script prepares a MySQL server for the project
 -- Creates a database named hbnb_dev_db if it doesn't already exist.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
- -- Creates a user named hbnb_dev with the password hbnb_dev_pwd if the user doesn't already exist.
+-- Creates a user named hbnb_dev with the password hbnb_dev_pwd if the user doesn't already exist.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grants all privileges on the hbnb_dev_db database to the user hbnb_dev when connecting from localhost.
