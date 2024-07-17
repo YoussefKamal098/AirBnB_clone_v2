82f5e1d6a9b34048f75c578589e0e6f5e180708d
YoussefKamal098 <kmalywsf098@gmail.com>
2024-07-18 00:13:52 +0300

    update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,53 +1,66 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Script to distribute an archive to web servers.
+
+This script uses Fabric to create a compressed archive of the web_static
+directory and distribute it to specified web servers.
+
+Dependencies:
+- Fabric (Install using: pip install fabric )
+    $ pip3 uninstall Fabric
+    $ sudo apt-get install libffi-dev
+    $ sudo apt-get install libssl-dev
+    $ sudo apt-get install build-essential
+    $ sudo apt-get install python3.4-dev or 3.7-dev
+    $ sudo apt-get install libpython3-dev
+    $ pip3 install pyparsing
+    $ pip3 install appdirs
+    $ pip3 install setuptools==40.1.0
+    $ pip3 install cryptography==2.8
+    $ pip3 install bcrypt==3.1.7
+    $ pip3 install PyNaCl==1.3.0
+    $ pip3 install Fabric3==1.14.post1
+
+Usage:
+1. Create the archive by running:
+   fab -f ./path/to/fab/file do_pack
+
+2. Deploy the created archive by running:
+   fab -f ./path/to/fab/file deploy_archive:/path/to/archive.tgz \
+   -u <username-on-remote-server> \
+   -i <path-to-public-key>
+
+Note:
+Replace '/path/to/archive.tgz' with the actual path to the archive
+created in step 1.
 """
-
 import os
-from datetime import datetime
 
-from fabric.api import *
+from fabric.api import run, put, env
 
-env.hosts = ["44.210.150.159", "35.173.47.15"]
-env.user = "ubuntu"
+env.hosts = ["web-01.realyousam.tech", "web-02.realyousam.tech"]
 
 
-def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
+# env.user = "ubuntu"  # replace with your username of the remote server
+# env.key_filename = "/path/to/public/key eg. ~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
-    """
-        Distribute archive.
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
-
-        print("New version deployed!")
+    """distributes an archive to the web servers"""
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    return False
+    except:
+        return False
