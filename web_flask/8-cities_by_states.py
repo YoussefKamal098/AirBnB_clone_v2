54dd40213b49fc8d8da3ca45e205309009a3744a
YoussefKamal098 <kmalywsf098@gmail.com>
2024-07-31 20:40:48 +0300

    update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -20,12 +20,31 @@ app.url_map.strict_slashes = False
 
 @app.route('/cities_by_states')
 def cities_by_states():
+    """
+    Route to list all states.
+
+    Fetches all states from the storage and passes them to
+    the template for rendering.
+
+    Returns:
+        Rendered HTML template displaying the list of states.
+    """
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def close_db(exc):
+    """
+    Remove the current SQLAlchemy session.
+
+    This function is called automatically when the
+    application context is torn down. It ensures that the
+    SQLAlchemy session is properly closed.
+
+    Args:
+        exc (Exception): The exception that caused the teardown, if any.
+    """
     storage.close()
 
 
