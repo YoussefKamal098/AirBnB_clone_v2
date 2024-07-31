c8a5f6dd5b9d337cf2f4374b44511acc0328d3eb
YoussefKamal098 <kmalywsf098@gmail.com>
2024-07-31 11:12:30 +0300

    update 3-python_route.py, 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,5 +30,10 @@ def c_is_fun(text='is cool'):
     return f'Python {text}'.replace('_', ' ')
 
 
+@app.route('/number/<int:n>')
+def c_is_fun(n):
+    return f'{n} is a number'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
